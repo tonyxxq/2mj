72aaacef09e3932a38f1e55ee3ecc640a57ef56c
xiexq <xiexq@edu-edu.com.cn>
2019-04-28 16:28:09 +0800

    啊啊啊

--- AIServer.java
+++ AIServer.java
@@ -36,12 +36,11 @@ public class AIServer extends WebSocketServer {
         System.out.println("收到了消息：" + message);
         List<Integer> gui = new ArrayList<>();
         List<Majiangs> majiangs = JSONObject.parseArray(message, Majiangs.class);
-
         double score = 0.0;
         int type = Majiangs.TYPE_ORIGIN;
         for (Majiangs mj : majiangs) {
             List<Integer> cards = MaJiangDef.stringToCards(mj.getOriginCards());
-            Integer card = MaJiangDef.stringToCards(mj.getCard()).get(0);
+            List<Integer> card = MaJiangDef.stringToCards(mj.getCard());
             if (mj.getType() == Majiangs.TYPE_OUT) { // 出牌
                 int out = AIUtil.outAI(cards, gui);
                 List<Integer> result = new ArrayList<>();
@@ -50,29 +49,27 @@ public class AIServer extends WebSocketServer {
                 System.out.println("输出的是: " + MaJiangDef.cardsToString(result));
                 return;
             } else if (mj.getType() == Majiangs.TYPE_PENG) { // 碰
-                double pengScore = AIUtil.pengAIScore(cards, gui, card, 1.0);
+                double pengScore = AIUtil.pengAIScore(cards, gui, card.get(0), 1.0);
                 if (pengScore > score) {
-                    score = pengScore;
                     type = Majiangs.TYPE_PENG;
                 }
             } else if (mj.getType() == Majiangs.TYPE_GANG) { // 杠
-                double gangScore = AIUtil.gangAIScore(cards, gui, card, 1.0);
+                double gangScore = AIUtil.gangAIScore(cards, gui, card.get(0), 1.0);
                 if (gangScore > score) {
-                    score = gangScore;
                     type = Majiangs.TYPE_GANG;
                 }
-            } else if (mj.getType() == Majiangs.TYPE_ORIGIN) { // 原始
+            } else if (mj.getType() == Majiangs.TYPE_ORIGIN) { // 原始的牌值
                 double originScore = AIUtil.calc(cards, gui);
                 if (originScore > score) {
-                    score = originScore;
                     type = Majiangs.TYPE_ORIGIN;
                 }
-            } else if (mj.getType() == Majiangs.TYPE_CHI_1 || mj.getType() == Majiangs.TYPE_CHI_2 || mj.getType() == Majiangs.TYPE_CHI_3) { // 吃
-                Integer card1 = MaJiangDef.stringToCards(mj.getCard1()).get(0);
-                Integer card2 = MaJiangDef.stringToCards(mj.getCard2()).get(0);
-                double chiScore = AIUtil.chiAIScore(cards, gui, card, card1, card2);
+            } else if (mj.getType() == Majiangs.TYPE_CHI_1
+                    || mj.getType() == Majiangs.TYPE_CHI_2
+                    || mj.getType() == Majiangs.TYPE_CHI_3) { // 吃牌
+                List<Integer> card1 = MaJiangDef.stringToCards(mj.getCard1());
+                List<Integer> card2 = MaJiangDef.stringToCards(mj.getCard2());
+                double chiScore = AIUtil.chiAIScore(cards, gui, card.get(0), card1.get(0), card2.get(0));
                 if (chiScore > score) {
-                    score = chiScore;
                     type = mj.getType();
                 }
             }
--- Majiangs.java
+++ Majiangs.java
@@ -11,41 +11,15 @@ public class Majiangs {
     public static final int TYPE_CHI = 3;
     public static final int TYPE_OUT = 4;
 
-    public static final int TYPE_CHI_1 = 5; // 吃牌在第一个位置
-    public static final int TYPE_CHI_2 = 6; // 吃牌在第二个位置
-    public static final int TYPE_CHI_3 = 7; // 吃牌在第三个位置
+    public static final int TYPE_CHI_1 = 5;
+    public static final int TYPE_CHI_2 = 6;
+    public static final int TYPE_CHI_3 = 7;
 
     private int type;
     private String originCards; // 玩家手上的牌
 
     private String card; // 需要碰、杠、胡的牌
     private String card1;
-    private String card2;
-
-    public void setType(int type) {
-        this.type = type;
-    }
-
-    public void setOriginCards(String originCards) {
-        this.originCards = originCards;
-    }
-
-    public void setCard(String card) {
-        this.card = card;
-    }
-
-
-    public int getType() {
-        return type;
-    }
-
-    public String getOriginCards() {
-        return originCards;
-    }
-
-    public String getCard() {
-        return card;
-    }
 
     public void setCard1(String card1) {
         this.card1 = card1;
@@ -61,5 +35,31 @@ public class Majiangs {
 
     public String getCard2() {
         return card2;
+    }
+
+    private String card2;
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setOriginCards(String originCards) {
+        this.originCards = originCards;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getOriginCards() {
+        return originCards;
+    }
+
+    public String getCard() {
+        return card;
     }
 }
